034c9acde4fdcd0d2bbc40a7850065379d0d3260
juan221093 <juanitoalvarez221093@gmail.com>
2020-02-17 20:33:26 -0700

    test

--- frontend/config.js
+++
@@ -1,9 +0,0 @@
-import getConfig from 'next/config'
-
-const {publicRuntimeConfig} =  getConfig()
-
-export const API = publicRuntimeConfig.PRODUCTION 
-    ? publicRuntimeConfig.API_PRODUCTION
-    :publicRuntimeConfig.API_DEVELOPMENT;
-    //'https://utilitesapp.com': 'http://localhost:8000';
-export const APP_NAME =  publicRuntimeConfig.APP_NAME;
